00187b8433cf1ff10778f96d38858c4ae751f045
lix <lix@localhost>
2020-05-13 11:24:02 +0300

    Registraton fixed

--- src/store/modules/profile.js
+++ src/store/modules/profile.js
@@ -179,7 +179,9 @@ const actions = {
         commit('removeCredentials');
         try {
             document.cookie = 'JSESSIONID=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-        }catch(e){}
+        }catch(e){
+            console.log(e);
+        }
         resolve(res);
       }
 
